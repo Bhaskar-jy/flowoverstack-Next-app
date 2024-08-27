b402c14b0e35d7dfa3cfe08f8f3d53f57300aa7b
Bhaskar-jy <000bhaskarsaikia@gmail.com>
2024-08-27 15:09:40 +0530

    second commit question collection

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-flowoverstack";
+export const questionCollection = "quesion";
+export const answerCollection = "answer";
+export const commentCollection = "comment";
+export const voteCollection = "vote";
+export const questionAttachmentBucket = "question-attachment";
---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,65 @@
+import { IndexType, Permission } from "node-appwrite";
+import { db, questionCollection } from "../name";
+import { databases } from "./config";
+
+export default async function createQuestionCollection() {
+  // create collection
+  await databases.createCollection(db, questionCollection, questionCollection, [
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.create("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ]);
+  console.log("Question COllection is Created");
+
+  //creating attributes and indexes
+
+  await Promise.all([
+    databases.createStringAttribute(db, questionCollection, "title", 100, true),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "content",
+      10000,
+      true
+    ),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "authorId",
+      50,
+      true
+    ),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "tags",
+      50,
+      true,
+      undefined,
+      true
+    ),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "attachmentId",
+      50,
+      false
+    ),
+  ]);
+  console.log("Question Attributes Created");
+
+  //Create Indeses
+
+  await Promise.all([
+    databases.createIndex(
+      db,
+      questionCollection,
+      "title",
+      IndexType.Fulltext,
+      ["title"],
+      ["asc"]
+    ),
+  ]);
+}
